60b2f0e6413ddd920f0a024ff74b84c2ba0bc941
ArielFJ <46905802+ArielFJ@users.noreply.github.com>
2020-11-11 10:26:32 -0400

    Push del proyecto

---
+++ Assets/imagenes de canvas/Porcentaje.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Porcentaje : MonoBehaviour
+{
+    private Slider slider; 
+   
+    public float porciento=1;
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+
+  
+    private void Update()
+    {
+        slider.value = porciento;
+    }
+
+
+
+
+}
