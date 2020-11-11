292353d88a5f6035f34019d7d67dd748b353e377
SaelusV <atonik34@gmail.com>
2020-11-11 11:37:05 -0400

    Correcciones en MainScene

---
+++ Assets/Scripts/Pac.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pac : MonoBehaviour
+{
+    public float tempInCelsius;
+    float tempInFaren;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempInFaren = (tempInCelsius * 1.8f) + 32f;
+    }
+
+    public float GetCelsius() 
+    {
+        return tempInCelsius;
+    }
+
+    public float GetFarenheit() 
+    {
+        return tempInFaren;  
+    }
+
+
+}
---
+++ Assets/Scripts/Termometro.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Termometro : MonoBehaviour
+{
+    public Transform firePoint;
+    public LineRenderer line;
+    public TextMeshProUGUI celciusText;
+    public TextMeshProUGUI farenText;
+    public GameObject canvas;
+    public Material screen;
+
+    public bool isGrabbeb;
+
+    private void Start()
+    {
+        canvas.SetActive(false);
+        screen.DisableKeyword("_EMISSION");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrabbeb)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StartCoroutine(Shoot());
+            }
+        }
+        if (!isGrabbeb) 
+        {
+            canvas.SetActive(false);
+            screen.DisableKeyword("_EMISSION");
+        }
+    }
+
+    IEnumerator Shoot() 
+    {
+        Debug.Log("shoot");
+        canvas.SetActive(true);
+        screen.EnableKeyword("_EMISSION");
+
+        RaycastHit hit;
+        if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, 50))
+        {
+            Debug.Log(hit.transform.name);
+
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, hit.point);
+
+            //Check si tiene script pciente
+            Pac pac = hit.transform.GetComponent<Pac>();
+            if (pac) 
+            {
+                //Busca Temperatura
+                //Escribe Temperaturas en el canvas
+                celciusText.text = pac.GetCelsius().ToString() + " °C";
+                farenText.text = pac.GetFarenheit().ToString()+ " °F";
+            }
+
+        }
+        else 
+        {
+            line.SetPosition(0, firePoint.position);
+            line.SetPosition(1, firePoint.position + firePoint.forward * 100);
+        }
+
+        line.enabled = true;
+        yield return new WaitForSeconds(1f);
+
+        line.enabled = false;
+
+
+    }
+}
