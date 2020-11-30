435595ca13560066386b01a45cbe1bd5ce569c80
Raulfcp <73600629+Raulfcp@users.noreply.github.com>
2020-11-30 09:33:51 -0400

    Restart/Off
    Botones

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void gameRestart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Restart");
+    }
+    public void gameEnd()
+    {
+        Application.Quit();
+        Debug.Log("GameOver");
+    }
+}
