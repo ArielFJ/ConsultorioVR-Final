3f8619d3b5d855817d4c2e972554848757b83b6a
SaelusV <atonik34@gmail.com>
2020-11-30 08:48:48 -0400

    Scene mANAGER
    PERMITE REINICIAR ESCENA Y SALIRDEL JUEGO

---
+++ Assets/SceneManage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void ReloadScene() 
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+}
